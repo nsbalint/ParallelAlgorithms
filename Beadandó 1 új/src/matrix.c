6fd8a160f52a7cdf17cc3641425c5e05ac6e649e
nsbalint <nsbalintps@gmail.com>
2024-05-15 11:13:48 +0200

    asd

--- Beadandó 1 új/src/matrix.c
+++ Beadandó 1 új/src/matrix.c
@@ -4,21 +4,19 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define MAX 20
-#define MAX_THREAD 20
+#define MAX 100
+#define MAX_THREAD 100
 
 int matA[MAX][MAX];
 int matB[MAX][MAX];
 int matC[MAX][MAX];
 int step_i = 0;
 
-// Struct for passing parameters to thread function
 typedef struct
 {
     int i;
 } thread_args;
 
-// Thread function
 void *worker(void *arg)
 {
     thread_args *args = (thread_args *)arg;
@@ -58,32 +56,57 @@ int main()
 {
     srand(time(NULL));
 
-    // Generating random values in matA and matB
-    for (int i = 0; i < MAX; i++)
+    int user_max, user_max_threads;
+    printf("Enter the maximum size for matrices (MAX): ");
+    scanf("%d", &user_max);
+    if (user_max > MAX)
     {
-        for (int j = 0; j < MAX; j++)
+        printf("Input exceeds the defined maximum size. Setting MAX to %d\n", MAX);
+        user_max = MAX;
+    }
+    if (user_max <= 0)
+    {
+        printf("Input cant be 0 or less.");
+        return 0;
+    }
+
+    printf("Enter the maximum number of threads (MAX_THREAD): ");
+    scanf("%d", &user_max_threads);
+    if (user_max_threads > MAX_THREAD)
+    {
+        printf("Input exceeds the defined maximum number of threads. Setting MAX_THREAD to %d\n", MAX_THREAD);
+        user_max_threads = MAX_THREAD;
+    }
+    if (user_max_threads <= 0)
+    {
+        printf("Input cant be 0 or less.");
+        return 0;
+    }
+
+    // Generating random values in matA and matB
+    for (int i = 0; i < user_max; i++)
+    {
+        for (int j = 0; j < user_max; j++)
         {
             matA[i][j] = rand() % 10;
             matB[i][j] = rand() % 10;
         }
     }
 
-    // Displaying matA
     printf("Matrix A\n");
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < user_max; i++)
     {
-        for (int j = 0; j < MAX; j++)
+        for (int j = 0; j < user_max; j++)
         {
             printf("%d ", matA[i][j]);
         }
         printf("\n");
     }
 
-    // Displaying matB
     printf("Matrix B\n");
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < user_max; i++)
     {
-        for (int j = 0; j < MAX; j++)
+        for (int j = 0; j < user_max; j++)
         {
             printf("%d ", matB[i][j]);
         }
@@ -91,20 +114,16 @@ int main()
     }
 
     clock_t start = clock();
+    pthread_t threads[user_max_threads];
 
-    // Declaring four threads
-    pthread_t threads[MAX_THREAD];
-
-    // Creating four threads, each evaluating its own part
-    for (int i = 0; i < MAX_THREAD; i++)
+    for (int i = 0; i < user_max_threads; i++)
     {
         thread_args *args = (thread_args *)malloc(sizeof(thread_args));
         args->i = step_i++;
         pthread_create(&threads[i], NULL, worker, (void *)args);
     }
 
-    // Joining and waiting for all threads to complete
-    for (int i = 0; i < MAX_THREAD; i++)
+    for (int i = 0; i < user_max_threads; i++)
     {
         pthread_join(threads[i], NULL);
     }
@@ -112,11 +131,10 @@ int main()
     clock_t end = clock();
     double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    // Displaying the result matrix
     printf("Multiplication of A and B\n");
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < user_max; i++)
     {
-        for (int j = 0; j < MAX; j++)
+        for (int j = 0; j < user_max; j++)
         {
             printf("%d ", matC[i][j]);
         }
